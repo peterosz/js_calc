6524ef32dd447a1d359faab198c1041432cf4eaf
peterosz <itharius@gmail.com>
2017-06-08 18:04:59 +0200

    fixed javascript link, started work on calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+var keys = document.getElementsByClassName('calculator span');
+var numbers = document.getElementsByClassName('numbers');
+var operators = document.getElementsByClassName('operators');
+
+for (i = 0; i < keys.length; i++) {
+    keys[i].onclick = function(e)
+    {
+        
+    }
+}
