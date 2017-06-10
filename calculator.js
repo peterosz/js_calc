75e1710b0443ded4014a422265dc4dcd424171e2
peterosz <itharius@gmail.com>
2017-06-10 11:25:36 +0200

    calculator works

--- calculator.js
+++ calculator.js
@@ -1,10 +1,60 @@
-var keys = document.getElementsByClassName('calculator span');
-var numbers = document.getElementsByClassName('numbers');
-var operators = document.getElementsByClassName('operators');
+var keys = document.querySelectorAll('.calculator span');
+var operators = ['+', '-', '*', '/', 'n*n'];
+var decimalAdded = false;
 
-for (i = 0; i < keys.length; i++) {
-    keys[i].onclick = function(e)
-    {
-        
-    }
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+		var input = document.querySelector('#screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		}
+		
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+								
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.innerHTML = eval(equation);
+				
+			decimalAdded = false;
+		}
+		
+		else if(operators.indexOf(btnVal) > -1) {
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+		
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		else {
+			input.innerHTML += btnVal;
+		}
+		if (inputVal.length > 15) {
+            alert('Screen is full!');
+            input.innerHTML = inputVal.substring(0,inputVal.length -2);
+        }
+	} 
 }
